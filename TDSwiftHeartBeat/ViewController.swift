c4a06d349e69e2597780a81d10d0acb9ad3e596e
michaeldai1006 <dtc74110@gmail.com>
2019-07-07 16:28:43 +0800

    Initial Commit

---
+++ TDSwiftHeartBeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDSwiftHeartBeat
+//
+//  Created by Michael Dai on 7/7/19.
+//  Copyright © 2019 TDSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
